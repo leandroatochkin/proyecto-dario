fb6abe06d29504bbaa3e9406bbc551e0dabb4813
leandroatochkin <leandro.atochkin@outlook.es>
2024-10-17 12:46:20 -0300

    update

--- backend/notification_mailer.cjs
+++ backend/notification_mailer.cjs
@@ -1,18 +1,19 @@
+require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const sendEmailNotification = (product) => {
     const transporter = nodemailer.createTransport({
       service: 'Gmail',
       auth: {
-        user: 'mail.de.prueba.para.app.restaurant@gmail.com',
-        pass: 'sico mbot xfbk poax',
+        user: process.env.MALBEC_EMAIL,
+        pass: process.env.MALBEC_EMAIL_PASS,
       },
     });
   
     const mailOptions = {
-      from: 'mail.de.prueba.para.app.restaurant@gmail.com',
-      to: 'leandronatochkin@gmail.com',
-      subject: 'Menu Updated',
+      from: process.env.MALBEC_EMAIL,
+      to: process.env.TEST_EMAIL,
+      subject: 'Nuevo Pedido',
       text: `${product.PD_cod_pro},${product.quantity},${product.address},${product.type},${product.total},${product.receptor}`,
     };
   
